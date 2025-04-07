81d6a74c2ff6e963af84ae572119637581a928fb
JeremyAlbertini <jeremy.albertini@epitech.eu>
2025-04-07 17:21:13 +0200

    add free_all before the exit

--- src/multicmd.c
+++ src/multicmd.c
@@ -31,8 +31,10 @@ int execute_multi_cmd(minishel_t **llenv, char *input)
     int status;
     char *token;
     char *ptr;
+    char *new = my_strdup(input);
 
-    token = strtok_r(input, ";", &ptr);
+    free(input);
+    token = strtok_r(new, ";", &ptr);
     while (token != NULL) {
         if (nbr_instr(token, '|'))
             status = executepipe(token, llenv);
@@ -40,7 +42,6 @@ int execute_multi_cmd(minishel_t **llenv, char *input)
             status = execute_main_cmd(token, llenv);
         token = strtok_r(NULL, ";", &ptr);
     }
-    free(input);
     return status;
 }
 
