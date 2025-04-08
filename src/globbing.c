dd5366fa031cd2e7694b3dee97e09e924dbb1ee7
Simon Puccio <simon.puccio@epitech.eu>
2025-04-08 14:40:01 +0200

    feat: search pattern research for special character

--- src/globbing.c
+++ src/globbing.c
@@ -10,17 +10,39 @@
 #include "minishel.h"
 #include <glob.h>
 
-int globbing(char *command)
+char *search_pattern(char **cmd)
+{
+    char *delim;
+
+    for (int i = 0; cmd[i]; i++) {
+        delim = strstr(cmd[i], "*");
+        if (delim)
+            return delim;
+        delim = strstr(cmd[i], "?");
+        if (delim)
+            return delim;
+        delim = strstr(cmd[i], "[");
+        if (delim && strstr(cmd[i], "]"))
+            return delim;
+    }
+    return NULL;
+}
+
+char **globbing(char **command)
 {
     char **match;
     glob_t g_struct;
-    int r = glob(command, GLOB_ERR, NULL, &g_struct);
+    char *pattern = search_pattern(command);
+    int r = 0;
 
+    if (!pattern)
+        return command;
+    r = glob(pattern, GLOB_ERR, NULL, &g_struct);
     if (r == 0) {
         match = g_struct.gl_pathv;
-        for (int i = 0; match[i]; i++)
-            printf("%s\n", match[i]);
+        globfree(&g_struct);
+        return match;
     }
     globfree(&g_struct);
-    return 0;
+    return NULL;
 }
